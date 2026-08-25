422cef3872da63843097796ad8edd97c0ce1baae
devacharan-cu <devacharan@gmail.com>
2026-08-25 03:41:19 +0000

    v2.0 Prototype Complete

--- android/app/src/main/java/com/onetap/reality/navigation/Screen.kt
+++
@@ -1,8 +0,0 @@
-package com.onetap.reality.navigation
-
-sealed class Screen(val route: String) {
-    data object Home : Screen("home")
-    data object Camera : Screen("camera")
-    data object Analysis : Screen("analysis")
-    data object Results : Screen("results")
-}
